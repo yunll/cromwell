b1ee2e75cf4e73b565f0864990c2fb8040f321f8
Miguel Covarrubias <mcovarr@broadinstitute.org>
2019-04-26 16:36:42 -0400

    fail louder

--- core/src/main/scala/cromwell/core/CromwellUserGuardianStrategy.scala
+++ core/src/main/scala/cromwell/core/CromwellUserGuardianStrategy.scala
@@ -1,11 +1,17 @@
 package cromwell.core
 
-import akka.actor.SupervisorStrategy.Escalate
+import akka.actor.SupervisorStrategy.{Escalate, Restart}
 import akka.actor.{ActorInitializationException, OneForOneStrategy, SupervisorStrategy, SupervisorStrategyConfigurator}
+import org.slf4j.LoggerFactory
 
 class CromwellUserGuardianStrategy extends SupervisorStrategyConfigurator {
+  val log = LoggerFactory.getLogger(classOf[CromwellUserGuardianStrategy])
   override def create(): SupervisorStrategy = OneForOneStrategy() {
     case _: ActorInitializationException => Escalate
-    case t => akka.actor.SupervisorStrategy.defaultDecider.applyOrElse(t, (_: Any) => Escalate)
+    case e: Exception =>
+      log.error("Actor terminating due to uncaught exception, initiating restart", e)
+      Restart
+    case t =>
+      SupervisorStrategy.defaultDecider.applyOrElse(t, (_: Any) => Escalate)
   }
 }
