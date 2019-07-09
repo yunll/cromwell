bc00f8704ccdeb67cf8c3d2b103b8d2624b802a8
TIANSHUN DING <dts@TIANSHUNdeMacBook-Pro.local>
2019-07-09 09:23:01 +0800

    fix

--- supportedBackends/vk/src/main/scala/cromwell/backend/impl/vk/VkConfiguration.scala
+++ supportedBackends/vk/src/main/scala/cromwell/backend/impl/vk/VkConfiguration.scala
@@ -3,8 +3,9 @@ package cromwell.backend.impl.vk
 import cromwell.backend.BackendConfigurationDescriptor
 
 class VkConfiguration(val configurationDescriptor: BackendConfigurationDescriptor) {
-  val kubeConf = configurationDescriptor.backendConfig.getString("kubeConf")
+  val apiServerUrl = configurationDescriptor.backendConfig.getString("apiserver-url")
   val namespace = configurationDescriptor.backendConfig.getString("namespace")
+  val token = configurationDescriptor.backendConfig.getString("token")
   val storagePath = Option(configurationDescriptor.backendConfig.getString("storagePath"))
   val runtimeConfig = configurationDescriptor.backendRuntimeAttributesConfig
 }
